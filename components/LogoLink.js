ada2fee3996b46b7f2d6a9d128004b39f7c45760
pavelhaj93 <pavel.hajduch@actumdigital.com>
2023-12-30 18:22:04 +0800

    replace the main logo

--- components/LogoLink.js
+++ components/LogoLink.js
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
-export default function LogoLink({className = '', ...rest}) {
-    return (
-        <Link href="/">
-            <a className={`inline-flex font-sans font-extrabold uppercase tracking-tighter text-4xl ${className}`} {...rest}>
-                Meize
-            </a>
-        </Link>
-    )
+export default function LogoLink({ logoColor, className = "", ...rest }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Link href="/">
+      <a
+        className={`inline-flex font-sans font-extrabold uppercase tracking-tighter text-4xl  ${className}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        {...rest}
+      >
+        {/* <Image
+          src={`/logos/meize-logo-${isHovered ? "orange" : logoColor}.svg`}
+          width={150}
+          height={100}
+        /> */}
+      </a>
+    </Link>
+  );
 }
