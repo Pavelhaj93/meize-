138505d354af8462e0233025bb068854539844a8
pavelhaj93 <pavel.hajduch@actumdigital.com>
2024-02-26 12:49:39 +0700

    fix: fix falling build on wrong properties on iframe player

--- pages/projects/sawsane.js
+++ pages/projects/sawsane.js
@@ -50,9 +50,9 @@ export default function AngusFarm({ project, prevProject, nextProject }) {
               width="100%"
               src="https://www.youtube.com/embed/NHLzTpb8DeM?si=SZplCsBb5X0Y_cDt"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             />
           </div>
 
