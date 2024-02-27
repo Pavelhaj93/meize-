94f2c87edb261a3d9f71db0735de3dbaf4bcc48a
pavelhaj93 <pavel.hajduch@actumdigital.com>
2024-02-27 12:58:52 +0700

    try to make og image bigger

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon/kelimek.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <meta property="og:image" content="/icons/kelimek.png " />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="200" />
 
         <link
           href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Mulish:wght@400;900&family=Work+Sans:wght@800;900&display=swap"
