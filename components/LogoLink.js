63d547416ac8919dec467167656c4ca40ed50b49
pavelhaj93 <pavel.hajduch@actumdigital.com>
2024-03-03 23:24:23 +0700

    fix nav logo

--- components/LogoLink.js
+++ components/LogoLink.js
@@ -3,12 +3,13 @@ import SvgLogo from "./svg/SvgLogo";
 
 export default function LogoLink({ logoColor, className = "", ...rest }) {
   return (
-    <Link
-      href="/"
-      className={`inline-flex font-sans font-extrabold uppercase tracking-tighter text-4xl mouse-hover:text-primary transition-colors duration-200 ${className}`}
-      {...rest}
-    >
-      <SvgLogo />
+    <Link href="/">
+      <button
+        className={`inline-flex font-sans font-extrabold uppercase tracking-tighter text-4xl mouse-hover:text-primary transition-colors duration-200 ${className}`}
+        {...rest}
+      >
+        <SvgLogo />
+      </button>
     </Link>
   );
 }
